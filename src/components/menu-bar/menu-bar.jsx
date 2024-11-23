92303420f2511123d0f05cd47fd6f91d7a31e93f
kokodev <github@kokofixcomputers.serv00.net>
2024-11-23 15:35:07 -0800

    Update menu-bar.jsx

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -922,7 +922,7 @@ class MenuBar extends React.Component {
                     <div className={styles.menuBarItem}>
                         <a
                             className={styles.feedbackLink}
-                            href="https://penguinmod.com"
+                            href="https://pm.kokodev.cc"
                             rel="noopener noreferrer"
                             target="_blank"
                         >
